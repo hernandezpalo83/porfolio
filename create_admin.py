90e142e776e4d0f64d55b51ec32a17360ce3b993
Jhernandez Mac <hernandezpalo@gmail.com>
2025-12-22 13:30:36 +0100

    ajuste del create admin

--- create_admin.py
+++ create_admin.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')  # Ajusta si tu settings está en otra ruta
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.config.settings') 
 django.setup()
 
 from django.contrib.auth import get_user_model
